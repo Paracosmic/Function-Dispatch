2d7a5dcded1f292fb54b4b404f1e8477b762b870
Paracosmic <45925426+Paracosmic@users.noreply.github.com>
2021-01-29 20:01:54 -0500

    Update MainTest.cpp
    -Added Comments
    
    -Added Member Functions to the test project

--- MainTest.cpp
+++ MainTest.cpp
@@ -10,13 +10,48 @@
 //
 #include <iostream>
 #include <string>
-void testFunc() { std::cout << "TEST FUNCTION SUCCESS" << std::endl; };
+void static_test_func() { std::cout << "STATIC TEST FUNCTION SUCCESS" << std::endl; };
+void static_test_func_int_param(int i) { std::cout << "STATIC TEST FUNCTION SUCCESS " << i  << std::endl; };
+
+class Foo 
+{
+public:
+	void member_test_func() { std::cout << "MEMBER TEST FUNCTION SUCCESS" << std::endl; };
+	void member_test_func_int_param(int i) { std::cout << "MEMBER TEST FUNCTION SUCCESS " << i << std::endl; };
+
+};
 int main() 
 {
 
 	VirtualFunctionUtility Virtual_Function_Util;
-	Virtual_Function_Util.Reflect_Static_Function("test", testFunc, {});
+	//Reflect static functions
+	Virtual_Function_Util.Reflect_Static_Function("static-test", static_test_func, {});
+	Virtual_Function_Util.Reflect_Static_Function("static-test-int", static_test_func_int_param, {});
 
-	Virtual_Function_Util.TryExecute("test");
+	Foo f;
+	//Reflect member functions
+	Virtual_Function_Util.Reflect_Member_Function("member-test",f, &Foo::member_test_func, {});
+	Virtual_Function_Util.Reflect_Member_Function("member-test-int",f, &Foo::member_test_func_int_param, {});
+
+	///try to execute function
+	//static
+	Virtual_Function_Util.TryExecute("static-test");
+	Virtual_Function_Util.TryExecute("static-test-int 1337");
+	//member
+	Virtual_Function_Util.TryExecute("member-test");
+	Virtual_Function_Util.TryExecute("member-test-int 1337");
+
+
+	//function command loop
+	//type the name of the function and the parameter values
+	while(true)
+	{ 
+		std::string input;		
+		std::getline ( std::cin, input);
+		Virtual_Function_Util.TryExecute(input);
+	
+	}
+
+
 	return 0;
 }
